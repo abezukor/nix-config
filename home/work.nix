f5f5599cada7f08594e9d147e40270f621c01f9d
Abraham Zukor <abe@maticrobots.com>
2025-11-19 17:44:26 -0800

    mdk config

--- home/work.nix
+++ home/work.nix
@@ -2,6 +2,23 @@
   home = {
     packages = with pkgs; [ slack ];
 
+    file = {
+      ".config/mdk/mdk.json" = {
+        text = ''
+          {
+              "environment": [
+                  "HISTFILE=/home/maticd/mdk_shared/bash_history"
+              ],
+              "volumes": [
+                  "/var/cache/home/maticd/.cache:/home/maticd/.cache",
+                  "~/mdk_shared:/home/maticd/mdk_shared",
+                  "~/.ssh:/home/maticd/.ssh"
+              ]
+          }
+        '';
+      };
+    };
+
     # You do not need to change this if you're reading this in the future.
     # Don't ever change this after the first build.  Don't ask questions.
     stateVersion = "25.05";
