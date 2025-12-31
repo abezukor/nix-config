c14dd791808aeecf27283425c37d9c9f0f6c2567
Abraham Zukor <abe@maticrobots.com>
2025-12-31 11:36:49 -0800

    docker: use cache drive

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -49,6 +49,10 @@
   boot.tmp = {
     useTmpfs = true;
   };
+  
+  virtualisation.docker.daemon.settings = {
+    data-root = "/var/cache/docker";
+  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
