8ce67a12a2106a4d5f2e4de70623dd82cb32a5a1
Abraham Zukor <abe@maticrobots.com>
2026-02-02 11:45:07 -0800

    system nixos 25.11

--- system/flake.nix
+++ system/flake.nix
@@ -3,20 +3,22 @@
 
   inputs = {
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-25.05";
+      url = "github:NixOS/nixpkgs/nixos-25.11";
     };
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations = {
-      abeMaticDesktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          ./nvidia.nix
-          ./matic.nix
-        ];
+  outputs =
+    { self, nixpkgs, ... }:
+    {
+      nixosConfigurations = {
+        abeMaticDesktop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./configuration.nix
+            ./nvidia.nix
+            ./matic.nix
+          ];
+        };
       };
     };
-  };
 }
